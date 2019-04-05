a8e2f9162a7fea5f3c02e150266ecc1820856933
Shady M. Najib <m.shady@gmail.com>
2019-04-05 19:27:37 +0200

    Display contact avatar, details and remove button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import ListContacts from "./ListContacts";
+
+const port = 9999;
+
+const contacts = [
+  {
+    id: "karen",
+    name: "Karen Isgrigg",
+    handle: "karen_isgrigg",
+    avatarURL: `http://localhost:${port}/karen.jpg`
+  },
+  {
+    id: "richard",
+    name: "Richard Kalehoff",
+    handle: "richardkalehoff",
+    avatarURL: `http://localhost:${port}/richard.jpg`
+  },
+  {
+    id: "tyler",
+    name: "Tyler McGinnis",
+    handle: "tylermcginnis",
+    avatarURL: `http://localhost:${port}/tyler.jpg`
+  }
+];
 
 class App extends Component {
   render() {
     return (
       <div>
-        Hello World
+        <ListContacts contacts={contacts} />
       </div>
     );
   }
