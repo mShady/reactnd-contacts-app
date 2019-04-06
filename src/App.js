b9780dc89d65fa30cce588f43c7848b963db882c
Shady M. Najib <m.shady@gmail.com>
2019-04-06 15:15:09 +0200

    Move contacts into App state

--- src/App.js
+++ src/App.js
@@ -3,32 +3,33 @@ import ListContacts from "./ListContacts";
 
 const port = 9999;
 
-const contacts = [
-  {
-    id: "karen",
-    name: "Karen Isgrigg",
-    handle: "karen_isgrigg",
-    avatarURL: `http://localhost:${port}/karen.jpg`
-  },
-  {
-    id: "richard",
-    name: "Richard Kalehoff",
-    handle: "richardkalehoff",
-    avatarURL: `http://localhost:${port}/richard.jpg`
-  },
-  {
-    id: "tyler",
-    name: "Tyler McGinnis",
-    handle: "tylermcginnis",
-    avatarURL: `http://localhost:${port}/tyler.jpg`
-  }
-];
-
 class App extends Component {
+  state = {
+    contacts: [
+      {
+        id: "karen",
+        name: "Karen Isgrigg",
+        handle: "karen_isgrigg",
+        avatarURL: `http://localhost:${port}/karen.jpg`
+      },
+      {
+        id: "richard",
+        name: "Richard Kalehoff",
+        handle: "richardkalehoff",
+        avatarURL: `http://localhost:${port}/richard.jpg`
+      },
+      {
+        id: "tyler",
+        name: "Tyler McGinnis",
+        handle: "tylermcginnis",
+        avatarURL: `http://localhost:${port}/tyler.jpg`
+      }
+    ]
+  };
   render() {
     return (
       <div>
-        <ListContacts contacts={contacts} />
+        <ListContacts contacts={this.state.contacts} />
       </div>
     );
   }
