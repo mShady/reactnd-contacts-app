4c331c549728f9d619a6665056a2e04799478346
Shady M. Najib <m.shady@gmail.com>
2019-04-12 20:02:24 +0200

    Filtering contacts based on search input field

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -28,26 +28,32 @@ class ListContacts extends Component {
           />
         </div>
         <ol className="contact-list">
-          {this.props.contacts.map(contact => (
-            <li key={contact.id} className="contact-list-item">
-              <div
-                className="contact-avatar"
-                style={{
-                  backgroundImage: `url(${contact.avatarURL})`
-                }}
-              />
-              <div className="contact-details">
-                <p>{contact.name}</p>
-                <p>{contact.handle}</p>
-              </div>
-              <button
-                className="contact-remove"
-                onClick={() => this.props.onDeleteContact(contact)}
-              >
-                Remove
-              </button>
-            </li>
-          ))}
+          {this.props.contacts
+            .filter(contact =>
+              contact.name
+                .toUpperCase()
+                .includes(this.state.query.toUpperCase())
+            )
+            .map(contact => (
+              <li key={contact.id} className="contact-list-item">
+                <div
+                  className="contact-avatar"
+                  style={{
+                    backgroundImage: `url(${contact.avatarURL})`
+                  }}
+                />
+                <div className="contact-details">
+                  <p>{contact.name}</p>
+                  <p>{contact.handle}</p>
+                </div>
+                <button
+                  className="contact-remove"
+                  onClick={() => this.props.onDeleteContact(contact)}
+                >
+                  Remove
+                </button>
+              </li>
+            ))}
         </ol>
       </div>
     );
