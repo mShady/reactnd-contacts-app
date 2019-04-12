9bbcd55c92a44ff8085fe403ac866f0dcd9d8fd3
Shady M. Najib <m.shady@gmail.com>
2019-04-12 20:59:36 +0200

    Adds "Now showing" with button to clear query

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -14,6 +14,11 @@ class ListContacts extends Component {
   };
 
   render() {
+    const { contacts } = this.props;
+    const filteredContacts = contacts.filter(contact =>
+      contact.name.toUpperCase().includes(this.state.query.toUpperCase())
+    );
+
     return (
       <div className="list-contacts">
         <div className="list-contacts-top">
@@ -27,33 +32,37 @@ class ListContacts extends Component {
             }}
           />
         </div>
+        {filteredContacts.length !== contacts.length ? (
+          <div className="showing-contacts">
+            <span>
+              Now showing {filteredContacts.length} of {contacts.length}
+            </span>
+            <button onClick={() => this.updateQuery("")}>Show all!</button>
+          </div>
+        ) : (
+          ""
+        )}
         <ol className="contact-list">
-          {this.props.contacts
-            .filter(contact =>
-              contact.name
-                .toUpperCase()
-                .includes(this.state.query.toUpperCase())
-            )
-            .map(contact => (
-              <li key={contact.id} className="contact-list-item">
-                <div
-                  className="contact-avatar"
-                  style={{
-                    backgroundImage: `url(${contact.avatarURL})`
-                  }}
-                />
-                <div className="contact-details">
-                  <p>{contact.name}</p>
-                  <p>{contact.handle}</p>
-                </div>
-                <button
-                  className="contact-remove"
-                  onClick={() => this.props.onDeleteContact(contact)}
-                >
-                  Remove
-                </button>
-              </li>
-            ))}
+          {filteredContacts.map(contact => (
+            <li key={contact.id} className="contact-list-item">
+              <div
+                className="contact-avatar"
+                style={{
+                  backgroundImage: `url(${contact.avatarURL})`
+                }}
+              />
+              <div className="contact-details">
+                <p>{contact.name}</p>
+                <p>{contact.handle}</p>
+              </div>
+              <button
+                className="contact-remove"
+                onClick={() => this.props.onDeleteContact(contact)}
+              >
+                Remove
+              </button>
+            </li>
+          ))}
         </ol>
       </div>
     );
